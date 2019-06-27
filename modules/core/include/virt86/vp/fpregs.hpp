984574a922a484d46c73c4be1792a88526f12ef3
StrikerX3 <ivan.rober@gmail.com>
2019-06-26 22:53:12 -0300

    FXSAVE and XSAVE data structures

--- modules/core/include/virt86/vp/fpregs.hpp
+++ modules/core/include/virt86/vp/fpregs.hpp
@@ -29,6 +29,10 @@ SOFTWARE.
 
 namespace virt86 {
 
+// Assert that float and double have the expected sizes in x86 platforms.
+static_assert(sizeof(float) == 4, "float is not 4 bytes long");
+static_assert(sizeof(double) == 8, "double is not 8 bytes long");
+
 // FPU control registers
 struct FPUControl {
     uint16_t cw;  // Control word
@@ -78,5 +82,225 @@ union MXCSR {
     };
     uint32_t u32;
 };
+
+// ST(#) register value
+struct FPValue {
+    uint64_t significand;
+    uint16_t exponentSign;
+    uint16_t /*padding*/ : 16;
+};
+static_assert(sizeof(FPValue) == 16);
+
+// XMM# register value
+union XMMValue {
+    int64_t i64[2];
+    int32_t i32[4];
+    int16_t i16[8];
+    int8_t i8[16];
+
+    double f64[2];
+    float f32[4];
+};
+static_assert(sizeof(XMMValue) == 16);
+
+// YMM#_H register value
+union YMMHighValue {
+    int64_t i64[2];
+    int32_t i32[4];
+    int16_t i16[8];
+    int8_t i8[16];
+
+    double f64[2];
+    float f32[4];
+};
+static_assert(sizeof(YMMHighValue) == 16);
+
+// YMM# register value
+union YMMValue {
+    struct {
+        XMMValue low;
+        YMMHighValue high;
+    } partitions;
+
+    int64_t i64[4];
+    int32_t i32[8];
+    int16_t i16[16];
+    int8_t i8[32];
+
+    double f64[4];
+    float f32[8];
+};
+static_assert(sizeof(YMMValue) == 32);
+
+// ZMM#_H register value
+union ZMMHighValue {
+    int64_t i64[4];
+    int32_t i32[8];
+    int16_t i16[16];
+    int8_t i8[32];
+
+    double f64[4];
+    float f32[8];
+};
+static_assert(sizeof(ZMMHighValue) == 32);
+
+// ZMM# register value
+union ZMMValue {
+    struct {
+        YMMValue low;
+        ZMMHighValue high;
+    } partitions;
+
+    int64_t i64[8];
+    int32_t i32[16];
+    int16_t i16[32];
+    int8_t i8[64];
+
+    double f64[8];
+    float f32[16];
+};
+static_assert(sizeof(ZMMValue) == 64);
+
+// FXSAVE area contents
+struct FXSAVEArea {
+    uint16_t fcw;
+    uint16_t fsw;
+    uint8_t ftw;
+    uint8_t /*reserved*/: 8;
+    uint16_t fop;
+    union {
+        struct {
+            uint32_t fip;
+            uint16_t fcs;
+            uint16_t /*reserved*/ : 16;
+        } ip32;
+        struct {
+            uint64_t fip;
+        } ip64;
+    };
+    union {
+        struct {
+            uint32_t fdp;
+            uint16_t fds;
+            uint16_t /*reserved*/ : 16;
+        } dp32;
+        struct {
+            uint64_t fdp;
+        } dp64;
+    };
+    MXCSR mxcsr;
+    MXCSR mxcsr_mask;
+    FPValue st_mm[8];
+    XMMValue xmm[16];
+    uint8_t _reserved[48];
+    uint8_t _unused[48];
+};
+constexpr auto s = sizeof(FXSAVEArea);
+static_assert(sizeof(FXSAVEArea) == 512, "FXSAVEArea struct is not 512 bytes long");
+
+// XSAVE area contents
+struct XSAVEArea {
+    FXSAVEArea fxsave;
+
+    // XSAVE header
+    struct {
+        union {
+            struct {
+                uint64_t
+                    x87           : 1,
+                    SSE           : 1,
+                    AVX           : 1,
+                    MPX_bndregs   : 1,
+                    MPX_bndcsr    : 1,
+                    AVX512_opmask : 1,
+                    ZMM_Hi256     : 1,
+                    Hi16_ZMM      : 1,
+                    PT            : 1,
+                    PKRU          : 1,
+                                  : 3,
+                    HDC           : 1;
+            } data;
+            uint64_t u64;
+        } xstate_bv;
+        union {
+            struct {
+                uint64_t
+                    x87           : 1,
+                    SSE           : 1,
+                    AVX           : 1,
+                    MPX_bndregs   : 1,
+                    MPX_bndcsr    : 1,
+                    AVX512_opmask : 1,
+                    ZMM_Hi256     : 1,
+                    Hi16_ZMM      : 1,
+                    PT            : 1,
+                    PKRU          : 1,
+                                  : 3,
+                    HDC           : 1,
+                                  : 50,
+                    format        : 1;
+            } data;
+            uint64_t u64;
+        } xcomp_bv;
+        uint8_t _reserved[48];
+    } header;
+};
+
+struct XSAVE_AVX {
+    YMMHighValue ymmHigh[16];
+};
+static_assert(sizeof(XSAVE_AVX) == 256);
+
+struct XSAVE_MPX_BNDREGS {
+    struct {
+        uint64_t low;
+        uint64_t high;
+    } bnd[4];
+};
+static_assert(sizeof(XSAVE_MPX_BNDREGS) == 64);
+
+struct XSAVE_MPX_BNDCSR {
+    uint64_t BNDCFGU;
+    uint64_t BNDSTATUS;
+};
+static_assert(sizeof(XSAVE_MPX_BNDCSR) == 16);
+
+struct XSAVE_AVX512_Opmask {
+    uint64_t k[8];
+};
+static_assert(sizeof(XSAVE_AVX512_Opmask) == 64);
+
+struct XSAVE_AVX512_ZMM_Hi256 {
+    ZMMHighValue zmmHigh[16];
+};
+static_assert(sizeof(XSAVE_AVX512_ZMM_Hi256) == 512);
+
+struct XSAVE_AVX512_Hi16_ZMM {
+    ZMMValue zmm[16];
+};
+static_assert(sizeof(XSAVE_AVX512_Hi16_ZMM) == 1024);
+
+struct XSAVE_PT {
+    uint64_t IA32_RTIT_CTL;
+    uint64_t IA32_RTIT_OUTPUT_BASE;
+    uint64_t IA32_RTIT_OUTPUT_MASK_PTRS;
+    uint64_t IA32_RTIT_STATUS;
+    uint64_t IA32_RTIT_CR3_MATCH;
+    uint64_t IA32_RTIT_ADDR0_A;
+    uint64_t IA32_RTIT_ADDR0_B;
+    uint64_t IA32_RTIT_ADDR1_A;
+    uint64_t IA32_RTIT_ADDR1_B;
+};
+static_assert(sizeof(XSAVE_PT) == 72);
+
+struct XSAVE_PKRU {
+    uint32_t pkru;
+};
+static_assert(sizeof(XSAVE_PKRU) == 4);
+
+struct XSAVE_HDC {
+    uint64_t IA32_PM_CTL1;
+};
+static_assert(sizeof(XSAVE_HDC) == 8);
 
 }
